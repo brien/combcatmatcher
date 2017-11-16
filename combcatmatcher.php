6fcfffd1b0977169d8806c79543595e35c42b38b
Brien Smith Martínez <bsmartinez@gmail.com>
2017-11-16 16:09:46 +0100

    Add module class

--- combcatmatcher.php
+++ combcatmatcher.php
@@ -2,6 +2,16 @@
 
 class CombCatMatcher extends Module
 {
+	public function __construct()
+	{
+		$this->name = 'combcatmatcher';
+		$this->tab = 'front_office_features';
+		$this->verison = '0.1';
+		$this->author = 'Brien Smith Martínez';
+		$this->displayName = 'Combination-Category Matcher';
+		$this->description = 'Module with overrides of CategoryController and Product Controller';
 
-	
+		parent::__construct();
+	}
+
 }
